9dc6792cb6ecf8de620afff495565275337258d6
leesewon <leesw1945@gmail.com>
2024-05-23 17:52:11 +0900

    JPA Auditing 적용

--- src/main/java/com/sparta/memo/MemoApplication.java
+++ src/main/java/com/sparta/memo/MemoApplication.java
@@ -2,7 +2,10 @@ package com.sparta.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// @EnableJpaAuditing를 꼭 달아야 Auditing 기능이 수행이 된다.
+@EnableJpaAuditing
 @SpringBootApplication
 public class MemoApplication {
 
